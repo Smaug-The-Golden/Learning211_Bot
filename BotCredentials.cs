797224e17f30274b354936c25eb10f3c01da38c8
Блоцкий Влад <Ultra_Mann@live.ru>
2022-01-16 22:06:16 +0300

    заготовка токена

---
+++ BotCredentials.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Learning211_Bot
+{
+    public static class BotCredentials
+    {
+        public static readonly string BotToken = "5002122812:AAEZibL9lPaZL5Z6LbgXKZfUug1SBfg4uI4";
+    }
+}
